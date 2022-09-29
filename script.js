bb96545cdfeae73a4f6a4ce23b1bc6ff7cf967c9
jinsungjun7 <jun.james.paul@gmail.com>
2022-09-29 18:49:33 -0500

    Created files and outlined core html/js fxn

---
+++ script.js
@@ -0,0 +1,32 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+
+function subtract(n1, n2) {
+    return n1 - n2;
+}
+
+function multiply(n1, n2) {
+    return n1*n2;
+}
+
+function divide(n1, n2) {
+    return (n1/n2);
+}
+
+function operate(operator, n1, n2) {
+    switch (operator) {
+        case '+':
+            return add(n1, n2);
+            break;
+        case '-':
+            return subtract(n1, n2);
+            break;
+        case '*':
+            return multiply(n1, n2);
+            break;
+        case '/':
+            return divide(n1, n2);
+            break;
+    }
+}
